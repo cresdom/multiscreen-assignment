947b6f3f5cb95bc5f77c484a3630fd4d92cd2c58
helaniedom <helanied18@gmail.com>
2026-02-21 19:07:36 -0700

    live streaming page mock up added

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,9 +2,8 @@ import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="stream/[id]" options={{ title: "Stream" }} />
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="(tabs)" />
     </Stack>
   );
 }
